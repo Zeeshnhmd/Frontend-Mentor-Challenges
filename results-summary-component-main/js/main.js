cfdce2672e1a12a4bce29ec2a3befc59062effba
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-07-13 23:46:50 +0530

    feat: results summary component

---
+++ results-summary-component-main/js/main.js
@@ -0,0 +1,30 @@
+import data from './data.js';
+const cardsContainer = document.querySelector('.cards__container');
+
+console.log(data);
+
+const cards = data
+	.map((el) => {
+		return `<div class="card ${
+			el.score === 80
+				? 'card__red'
+				: el.score === 92
+				? 'card__yellow'
+				: el.score === 61
+				? 'card__green'
+				: el.score === 72
+				? 'card__purple'
+				: ''
+		}">
+    <div class="card__right__side">
+       <img src='${el.icon}' alt='${el.category}' class="icon"/>
+       <p class="card__right__side__name">${el.category}</p>
+    </div>
+     <p class="card__score">
+       ${el.score} <span class="card__score__out__of">/ 100</span>
+     </p>
+  </div>`;
+	})
+	.join('');
+
+cardsContainer.innerHTML = cards;
